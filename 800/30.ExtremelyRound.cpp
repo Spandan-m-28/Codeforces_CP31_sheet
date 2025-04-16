5d7b3259ad32fe2b45c87e87ddc7822045fc2513
Spandan Meshram <meshramspandan28@gmail.com>
2025-04-16 18:15:05 +0530

    800 rating comlleted

--- 800/30.ExtremelyRound.cpp
+++ 800/30.ExtremelyRound.cpp
@@ -1,17 +1,29 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-void solve(){
-    
-}
+void solve() {
+    int n;
+    cin >> n;
+    int count = 0;
 
-int main(){
-
-    int t;
-    cin >> t;
-    while(t--){
-        solve();
+    // Try all base digits 1 to 9
+    for (int d = 1; d <= 9; ++d) {
+        int val = d;
+        // Multiply by 10 until we exceed n
+        while (val <= n) {
+            count++;
+            val *= 10;
+        }
     }
 
+    cout << count << endl;
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
     return 0;
 }
---
+++ 800/31.TwoPermutations.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    int n,a,b;
+    cin >> n >> a >> b;
+    if(a + b + 2 <= n || a == b && b == n){
+        cout << "Yes" << endl;
+    }else{
+        cout << "No" << endl;
+    }
+}
+
+int main(){
+
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+
+    return 0;
+}
