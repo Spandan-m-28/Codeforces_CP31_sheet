2debe45c9876d8fc6421447a155ccc86803be7c1
Spandan-m <meshramspandan28@gmail.com>
2025-10-09 18:48:41 +0530

    solved 1 question

--- 1100/18. Teleporters (Easy Version).cpp
+++ 1100/18. Teleporters (Easy Version).cpp
@@ -4,7 +4,30 @@ using namespace std;
 typedef long long ll;
 
 void solve(){
-    
+    int n ,c;
+    cin >> n >> c;
+    vector<int> arr(n);
+    priority_queue<int,vector<int>,greater<int>> pq;
+    for(int i = 0;i < n;i++){
+        int x;
+        cin >> x;
+        pq.push(x + i + 1);
+    }
+
+    int currCoins = c;
+    int teleports = 0;
+    while(!pq.empty()){
+        int price = pq.top();
+        pq.pop();
+
+        if(price <= currCoins){
+            teleports++;
+            currCoins -= price;
+        }
+    }
+
+    cout << teleports << endl;
+
 }
 
 int main(){
