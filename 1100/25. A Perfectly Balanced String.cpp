a86844c639f855ac00fc6a4586346f445172a603
Spandan-m <meshramspandan28@gmail.com>
2025-10-21 09:12:35 +0530

    solved some question

--- 1100/25. A Perfectly Balanced String.cpp
+++ 1100/25. A Perfectly Balanced String.cpp
@@ -1,27 +1,37 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-typedef long long ll;
-ll mod = 1e9 + 7;
+void solve() {
+	string s;
+	cin >> s; 
+	int n = (int)s.size(); 
+	set<char> c; 
+	int k;
 
-void solve(){
-    //int n;
-    //cin >> n;
+	for (k = 0; k < n; k++) { 
+		if (c.find(s[k]) == c.end()) {
+			c.insert(s[k]); 
+		} else {
+			break;
+		}
+	}
 
-    // vector<ll> arr(n);
-    // for(int i = 0;i < n;i++){
-    //     cin >> arr[i];
-    // }
+	for (int i = k; i < n; i++) { 
+		if (s[i] != s[i - k]) {
+			cout << "NO" << endl; 
+			return;
+		}
+	}
+	cout << "YES" << endl;
 }
 
-int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    int t;
-    cin >> t;
-    while(t--){
-        solve();
-    }
-    return 0;
+int main() {
+#ifndef ONLINE_JUDGE
+	freopen("Error.txt", "w", stderr);
+#endif
+	int t = 1;
+	cin >> t; // Read the number of test cases
+	while (t--) {
+		solve(); // Solve each test case
+	}
 }
